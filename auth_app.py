f25b73ff0fc85d0d2c62b71f448e949c1cfe28d9
Andrew Elwood <amelwood01@gmail.com>
2025-07-07 21:17:03 +0100

    Added funtion to request token from access code but there is a bug

--- auth_app.py
+++ auth_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, request
+import requests
 from dotenv import load_dotenv
 import os
 
@@ -7,6 +8,7 @@ load_dotenv()
 
 # Get credentials from .env
 CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
+CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
 REDIRECT_URI = os.getenv("STRAVA_REDIRECT_URI")
 
 # Create flask app
@@ -25,3 +27,24 @@ def home():
         f"&scope=activity:read"
     )
     return f'<a href="{auth_url}">Connect to Strava</a>'
+
+
+@app.route("/authorised")
+def authorised():
+    ACCESS_CODE = request.args.get("code")
+    # Send request to https://www.strava.com/oauth/token
+    tokenReqURL = "https://www.strava.com/oauth/token"
+    tokenReqResponse = requests.post(
+        tokenReqURL,
+        data={
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "code": ACCESS_CODE,
+            "grant_type": "authorization_code",
+        },
+    )
+    return f"Access Code: {ACCESS_CODE} \n Response JSON: {tokenReqResponse.json()}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
