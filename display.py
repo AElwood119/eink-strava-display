f698026c1d422a7a8cea8561d8cd221f54dac787
Andrew Elwood <amelwood01@gmail.com>
2026-04-25 12:25:39 +0100

    started to pull together a pillow image as my dahsboard display. Not finished and some errors but a start.

--- display.py
+++ display.py
@@ -1,1 +1,13 @@
-from PIL import Image
+from PIL import Image, ImageDraw
+import sys
+
+# Create an image
+eink_resolution = (800, 480)  # in pixels (this is for 7.3 in inky impression)
+
+with Image.open("display_image.png") as im:
+    draw = ImageDraw.Draw(im)
+    draw.line((0, 0) + im.size, fill=128)
+    draw.line((0, im.size[1], im.size[0], 0), fill=128)
+
+    # write to stdout
+    im.save(sys.stdout, "PNG")
