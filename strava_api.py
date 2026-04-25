007af57558e1677577ffea2a820d0d71939a6af6
Andrew Elwood <amelwood01@gmail.com>
2026-04-25 10:45:11 +0100

    strava_api.py now saves the query response of activities to activity_responses.json for offline dev rather than querying each time

--- strava_api.py
+++ strava_api.py
@@ -1,6 +1,6 @@
 import json
 import requests
-
+import os
 
 
 def get_activities(access_token):
@@ -8,23 +8,31 @@ def get_activities(access_token):
     headers = {"Authorization": f"Bearer {access_token}"}
     response = requests.get(url=url, headers=headers)
     if response.status_code == 200:
+        print("RESPONSE: \n", response.json)
         # Successful response returns the json data.
         return response.json()
     else:
         # Something went wrong so return None
         return None
-    
-if __name__ =="__main__":
+
+
+if __name__ == "__main__":
     # Read access token from strava_tokens.json
-    with open('strava_tokens.json', 'r') as tokens_file:
+    with open("strava_tokens.json", "r") as tokens_file:
         data = json.load(tokens_file)
         most_recent_tokens = data[-1]
 
-    access_token = most_recent_tokens['access_token']
+    access_token = most_recent_tokens["access_token"]
 
     activities = get_activities(access_token=access_token)
     if activities:
         for activity in activities[:5]:  # just look at the first 5 for now
+            print(activity["type"])
             print(activity["name"], "-", activity["distance"], "meters")
+
+            # For the purposes of testing, we save the json response to file
+            # Load existing tokens or start a new list
+            with open("activity_responses.json", "w") as f:
+                json.dump(activities, f, indent=2)
     else:
         print("Failed to fetch activities.")
