982cb85762df5a71aadd9ad4d31186e8a5befa19
Andrew Elwood <amelwood01@gmail.com>
2026-04-29 21:33:42 +0100

    Display updates to add boxes and a header and some better formatting

--- display.py
+++ display.py
@@ -9,7 +9,10 @@ running_stats = get_running_stats(activities=activities, numDays=7)
 
 ## Layout
 metrics_x = 20
-metrics_y = 20
+metrics_y = 100
+
+header_x = 20
+header_y = 20
 
 # Create an image
 eink_resolution = (800, 400)  # in pixels (this is for 7.3 in inky impression)
@@ -23,10 +26,12 @@ draw = ImageDraw.Draw(base_image)
 # Define fonts
 label_font = ImageFont.truetype("fonts/FjallaOne-Regular.ttf", 24)
 value_font = ImageFont.truetype("fonts/FjallaOne-Regular.ttf", 60)
+header_font = ImageFont.truetype("fonts/FjallaOne-Regular.ttf", 45)
 
 ## Strings for displaying
 dist_string = f"Total Distance = {running_stats[0] / 1000:.1f} km"
 climb_string = f"Total Climb = {running_stats[1]:.0f} m"
+header_string = f"Strava Weekly Mileage"
 
 draw.text((metrics_x, metrics_y), "Distance", font=label_font, fill=text_colour)
 draw.text(
@@ -36,13 +41,19 @@ draw.text(
     fill=text_colour,
 )
 
-draw.text((metrics_x + 300, metrics_y), "Climb", font=label_font, fill=text_colour)
+
+draw.text((header_x, header_y), header_string, font=header_font, fill=text_colour)
+draw.text((metrics_x, metrics_y + 150), "Climb", font=label_font, fill=text_colour)
 draw.text(
-    (metrics_x + 300, metrics_y + 30),
+    (metrics_x, metrics_y + 170),
     f"{running_stats[1]:.0f} m",
     font=value_font,
     fill=text_colour,
 )
+
+draw.rectangle((10, 20, 780, 80), outline=0)  # header
+draw.rectangle((10, 100, 320, 360), outline=0)  # metrics
+draw.rectangle((340, 100, 780, 360), outline=0)  # plot
 
 
 base_image.show()
